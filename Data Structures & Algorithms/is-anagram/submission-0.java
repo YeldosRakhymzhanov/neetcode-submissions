25009364856ab61fdfb72e6cfc0dd2319a5f154b
YeldosRakhymzhanov <rakhimzhanov.eldos@gmail.com>
2026-05-30 22:12:21 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        
+        for(char c : s.toCharArray()){
+            count[c - 'a']++;
+        }
+        for(char c : t.toCharArray()){
+            count[c - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public void main(String[] args){
+        
+        String s = "racecar";
+        String t = "carrace";
+        System.out.println(isAnagram(s, t));
+    }
+}
